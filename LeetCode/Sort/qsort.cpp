41d2d599539ec8cabed5cfb62b4d0244f1a38fd8
uwadeearly <zhaowei200701@163.com>
2022-01-18 20:08:55 +0800

    quick sort two

--- LeetCode/Sort/qsort.cpp
+++ LeetCode/Sort/qsort.cpp
@@ -6,14 +6,14 @@
 /*   By: wei.zhao <Wei.Zhao@stream_computing.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/16 16:25:18 by wei.zhao          #+#    #+#             */
-/*   Updated: 2021/11/17 10:18:30 by wei.zhao         ###   ########.fr       */
+/*   Updated: 2022/01/18 20:08:03 by wei.zhao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 using namespace std;
 
-//inside core execute
+// inside core execute
 int partition(int *arr, int left, int right) {
   int index = left;
   int pivot = arr[index];
@@ -27,31 +27,51 @@ int partition(int *arr, int left, int right) {
   return index;
 }
 
-
 // core 0 execute
 void qsort(int *arr, int left, int right) {
-  if(left>= right)
+  if (left >= right)
     return;
   int index = partition(arr, left, right);
   cout << "index: " << index << endl;
-  
+
   qsort(arr, left, index - 1);
   qsort(arr, index + 1, right);
+}
+
+void quick_sort(int *arr, int left, int right) {
+  if (left >= right)
+    return;
+  int first = left, last = right, base = arr[first];
+
+  while (first < last) {
+    while (first < last && base >= arr[last]) {
+      last--;
+    }
+    arr[first] = arr[last];
+    while (first < last && base <= arr[first]) {
+      first++;
+    }
+    arr[last] = arr[first];
+  }
+  arr[first] = base;
+
+  quick_sort(arr, left, first - 1);
+  quick_sort(arr, first + 1, right);
 }
 
 int main() {
   int arr[] = {0, 3, 1, 5, 4, 9, 8, 7, 2, 6};
   int arg_sort[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-  qsort(arr, 0, 9);
-  
+  // qsort(arr, 0, 9);
+  quick_sort(arr, 0, 9);
+
   for (auto &elem : arr) {
     cout << elem << ", ";
   }
   cout << endl;
-  
-  for(auto & index: arg_sort){
-    cout << index << ", ";
-  }
-  cout << endl;
-  
+
+  // for (auto &index : arg_sort) {
+  //   cout << index << ", ";
+  // }
+  // cout << endl;
 }
