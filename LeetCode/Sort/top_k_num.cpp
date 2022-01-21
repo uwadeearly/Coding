b258c7314a38c3970322e51e1e1a5a60ccc32fd8
uwadeearly <zhaowei200701@163.com>
2022-01-21 16:35:49 +0800

    top k

--- LeetCode/Sort/top_k_num.cpp
+++ LeetCode/Sort/top_k_num.cpp
@@ -6,7 +6,7 @@
 /*   By: wei.zhao <Wei.Zhao@stream_computing.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/19 19:55:49 by wei.zhao          #+#    #+#             */
-/*   Updated: 2022/01/19 20:22:00 by wei.zhao         ###   ########.fr       */
+/*   Updated: 2022/01/21 16:35:10 by wei.zhao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ int top_k_num(vector<int> &arr, int k) {
         count++;
       }
     }
-    if (count == (k-1)) {
+    if (count == (k - 1)) {
       ret = arr[i];
       break;
     }
@@ -37,9 +37,40 @@ int top_k_num(vector<int> &arr, int k) {
   return ret;
 }
 
+int top_k_(vector<int> &arr, int left, int right, int k) {
+  int i = left, j = right, ret;
+  int base = arr[left];
+  while (i < j) {
+    while (i < j && base > arr[j]) {
+      --j;
+    }
+    arr[i] = arr[j];
+
+    while (i < j && base < arr[i]) {
+      ++i;
+    }
+    arr[j] = arr[i];
+  }
+  arr[i] = base;
+  
+  if (k-1 == i) {
+    return arr[i];
+  } else if (k-1 > i) {
+    ret = top_k_(arr, i + 1, right, k);
+  } else {
+    ret = top_k_(arr, left, i - 1, k);
+  }
+  return ret;
+}
+
 int main() {
   vector<int> arr = {3, 2, 1, 5, 6, 4};
-  int ret = top_k_num(arr, 2);
+  // int ret = top_k_num(arr, 2);
+  int ret = top_k_(arr, 0, 5, 2);
   cout << "result: " << ret << endl;
+  for(auto& elem: arr){
+    cout << elem << ", ";
+  }
+  cout << endl;
   return 0;
 }
