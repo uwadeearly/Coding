a96699d18980a688359a2f0415e8281e39c32e57
uwadeearly <zhaowei200701@163.com>
2022-02-11 20:37:45 +0800

    binary tree second max

--- LeetCode/BinaryTree/base.cpp
+++ LeetCode/BinaryTree/base.cpp
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   source.cpp                                         :+:      :+:    :+:   */
+/*   base.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: wei.zhao <Wei.Zhao@stream_computing.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/13 13:50:45 by wei.zhao          #+#    #+#             */
-/*   Updated: 2021/11/13 14:56:47 by wei.zhao         ###   ########.fr       */
+/*   Updated: 2022/02/11 20:20:30 by wei.zhao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -75,14 +75,14 @@ void InOrderNoRec(Node *node) {
 
   node_s.push(node);
   node = node->left;
-  
+
   while (!node_s.empty() || node) {
     while (node) {
       node_s.push(node);
       node = node->left;
     }
-    
-    cout << node_s.top()->val << ", ";    
+
+    cout << node_s.top()->val << ", ";
     node = node_s.top()->right;
     node_s.pop();
   }
@@ -90,8 +90,8 @@ void InOrderNoRec(Node *node) {
 
 /*post order travseral*/
 /*recursion*/
-void PostOrderRec(Node* node){
-  if(node == nullptr)
+void PostOrderRec(Node *node) {
+  if (node == nullptr)
     return;
   PostOrderRec(node->left);
   PostOrderRec(node->right);
@@ -99,15 +99,39 @@ void PostOrderRec(Node* node){
 }
 
 /*loop no recursion*/
-void PostOrderNoRec(Node* node){
-  if(node == nullptr)
+void PostOrderNoRec(Node *node) {}
+
+void secondMax(Node *node, int* ret) {
+  static int nums;
+  if (nums == 2 || node == nullptr) {
     return;
-  
-  stack<Node*> node_s;
-  node_s.push(node);
-  
-  
-  
+  } else {
+    ++nums;
+    secondMax(node->right, ret);
+    *ret = node->val;
+    secondMax(node->left, ret);
+  }
 }
 
-int main() { return 0; }
+int max_depth(Node *node) {
+  if (node == nullptr)
+    return 0;
+ 
+  int left = 1 + max_depth(node->left);
+  int right = 1 + max_depth(node->right);
+  return left > right ? left : right;
+}
+
+int main() {
+  Node* Tree = new Node;
+  Tree->val = 11;
+  Tree->left = new Node;
+  Tree->left->val = 10;
+  
+  Tree->right = new Node;
+  Tree->right->val = 12;
+  int ret=0;
+  secondMax(Tree, &ret);
+  cout << "ret: " << ret << endl;
+     
+  return 0; }
