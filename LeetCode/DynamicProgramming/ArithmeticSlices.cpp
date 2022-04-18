c0c79576baa757ab841c1e1add3d079339a658c0
uwadeearly <zhaowei200701@163.com>
2022-04-18 14:24:38 +0800

    dp arithmetic

---
+++ LeetCode/DynamicProgramming/ArithmeticSlices.cpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ArithmeticSlices.cpp                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wei.zhao <Wei.Zhao@stream_computing.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/15 19:45:20 by wei.zhao          #+#    #+#             */
+/*   Updated: 2022/04/15 19:49:09 by wei.zhao         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+/*
+ * 给定一个数组，求这个数组中连续且等差的子数组一共有多少个。
+ * 输入是一个一维数组，输出是满足等差条件的连续字数组个数。
+ * Input: nums = [1,2,3,4]
+ * Output: 3
+ * 在这个样例中，等差数列有[1,2,3]、[2,3,4] 和[1,2,3,4]
+*/
+
+int arithmetic_slices(vector<int>& inputs){
+  int len = inputs.size();
+  if(len < 3) return 0;
+
+  
+}
+
+int main(){
+  return 0;
+}
