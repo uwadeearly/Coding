f42b25ef764d007ca882c41442c790ece7e76f97
uwadeearly <zhaowei200701@163.com>
2022-06-21 19:54:54 +0800

    start dp

--- LeetCode/DynamicProgramming/WordBreak.cpp
+++ LeetCode/DynamicProgramming/WordBreak.cpp
@@ -26,6 +26,7 @@ bool get_word_break(string str, vector<string>& word_dict){
   int len = str.length();
   vector<bool> dp(len+1, false);
   dp[0] = true;
+
   for(int i=1; i<=len; ++i){
     for(auto& word : word_dict){
       int word_len = word.length();
